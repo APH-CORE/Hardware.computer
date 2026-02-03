5e4386c71e3b4fcf29ed6fb7c017177e29e52783
APH-CORE <daffahadiana08@gmail.com>
2026-02-03 18:14:24 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function showInfo(hardware) {
+  let info = document.getElementById("info");
+
+  if (hardware === "cpu") {
+    info.innerHTML = "<h2>CPU</h2><p>CPU adalah otak komputer yang mengolah semua perintah.</p>";
+  } 
+  else if (hardware === "gpu") {
+    info.innerHTML = "<h2>GPU</h2><p>GPU berfungsi untuk mengolah grafis dan tampilan visual.</p>";
+  } 
+  else if (hardware === "ram") {
+    info.innerHTML = "<h2>RAM</h2><p>RAM menyimpan data sementara agar komputer bekerja lebih cepat.</p>";
+  }
+}
